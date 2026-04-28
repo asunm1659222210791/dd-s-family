1876cc26435dd946eabde7040ce085223beb24c0
devadharshini <dd996293.04@gmail.com>
2026-04-28 10:10:31 +0530

    dad extra details

--- family.js
+++ family.js
@@ -2,8 +2,3 @@ console.log("Welcome to our Family Project");
 
 console.log("Dad: Created the main house (Main Branch)");
 
-console.log("Mom: Added Kitchen (mom-work branch)");
-
-console.log("DD: Added Study Room (dd-work branch)");
-
-console.log("All work combined after merge");
